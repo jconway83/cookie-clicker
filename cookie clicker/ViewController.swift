96f6c21096e31cf113c50ad98117fbde827ab51f
Jet Conway <jconway@bsd220.org>
2020-02-24 11:42:58 -0600

    Initial Commit

---
+++ cookie clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cookie clicker
+//
+//  Created by Jet Conway on 2/24/20.
+//  Copyright © 2020 Jet Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
